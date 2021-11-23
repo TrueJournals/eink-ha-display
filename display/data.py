e90182272e7a1ca4c5d3e741f618ad05f808bdd8
Bobby Graese <bobby@grae.se>
2021-11-22 21:12:27 -0600

    Initial commit of code

---
+++ display/data.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import List, Any, Optional
+
+
+@dataclass
+class DisplayData:
+    electric_cost: float = 0.0
+    forecast: List[Any] = field(default_factory=lambda: [])
+    day_lowhigh: List[float] = field(default_factory=lambda: [float("nan"), float("nan")])
+    day_lowhigh_date: Optional[datetime] = None
+    daily_energy: float = 0.0
+
