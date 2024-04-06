060b4451004699f57ed6a3a2740e73cb3a6fc5cf
Bobby Graese <bobby@grae.se>
2024-04-06 08:47:49 -0500

    Switch forecast to websocket, since REST doesn't work for forecast anymore

--- display/drawer.py
+++ display/drawer.py
@@ -13,7 +13,8 @@ logger = logging.getLogger(__name__)
 
 def load_icon(filename, width=None, height=None):
     with BytesIO() as out:
-        cairosvg.svg2png(url=f"icons/{filename}", write_to=out, background_color="white", output_width=width, output_height=height)
+        cairosvg.svg2png(url=f"icons/{filename}", write_to=out,
+                         background_color="white", output_width=width, output_height=height)
         im = Image.open(out)
         im.load()
         return im
@@ -27,7 +28,8 @@ class DisplayDrawer:
         self.height = height
 
     def _load_static_data(self):
-        self._font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 16)
+        self._font = ImageFont.truetype(
+            '/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 16)
         self._icon_lightning = load_icon("lightning-bolt.svg")
         self._icon_thermo_high = load_icon("thermometer-chevron-up.svg")
         self._icon_thermo_low = load_icon("thermometer-chevron-down.svg")
@@ -39,7 +41,7 @@ class DisplayDrawer:
 
     def _get_icon(self, weather):
         for icon in self._icons:
-            if icon['name'] == weather or icon['name']  == f"weather-{weather}":
+            if icon['name'] == weather or icon['name'] == f"weather-{weather}":
                 return icon["name"]
             if weather in icon['aliases'] or f"weather-{weather}" in icon['aliases']:
                 return icon["name"]
@@ -68,7 +70,8 @@ class DisplayDrawer:
     def _draw_plant_moisture(self, image, draw, x_pos):
         image.paste(self._icon_plant_moisture, (x_pos, 2))
         x_pos += 25
-        moisture = "--" if math.isnan(self.data.soil_moisture) else self.data.soil_moisture
+        moisture = "--" if math.isnan(
+            self.data.soil_moisture) else self.data.soil_moisture
         draw.text((x_pos, 5), f"{moisture}%", font=self._font, fill=0)
         x_pos += int(draw.textlength(f"{moisture}%", font=self._font))
         return x_pos
@@ -76,7 +79,8 @@ class DisplayDrawer:
     def _draw_low_temperature(self, image, draw, x_pos):
         image.paste(self._icon_thermo_low, (x_pos, 2))
         x_pos += 25
-        low_temp = "--" if math.isnan(self.data.day_lowhigh[0]) else self.data.day_lowhigh[0]
+        low_temp = "--" if math.isnan(
+            self.data.day_lowhigh[0]) else self.data.day_lowhigh[0]
         draw.text((x_pos, 5), f"{low_temp}°", font=self._font, fill=0)
         x_pos += int(draw.textlength(f"{low_temp}°", font=self._font))
         x_pos += 10
@@ -85,7 +89,8 @@ class DisplayDrawer:
     def _draw_high_temperature(self, image, draw, x_pos):
         image.paste(self._icon_thermo_high, (x_pos, 2))
         x_pos += 25
-        high_temp = "--" if math.isnan(self.data.day_lowhigh[1]) else self.data.day_lowhigh[1]
+        high_temp = "--" if math.isnan(
+            self.data.day_lowhigh[1]) else self.data.day_lowhigh[1]
         draw.text((x_pos, 5), f"{high_temp}°", font=self._font, fill=0)
         x_pos += int(draw.textlength(f"{high_temp}°", font=self._font))
         return x_pos
@@ -97,8 +102,10 @@ class DisplayDrawer:
                 draw.rounded_rectangle(((x_pos, 30), (x_pos + 70, 120)), 7)
             except AttributeError:
                 draw.rectangle(((x_pos, 30), (x_pos + 70, 120)))
-            draw.text((x_pos + (70/2), 32), f"{forecast_time:%l %p}", font=self._font, fill=0, anchor="ma")
-            draw.text((x_pos + 3 + (70/2), 120), f"{hour['temperature']}°", font=self._font, fill=0, anchor="md")
+            draw.text((x_pos + (70/2), 32),
+                      f"{forecast_time:%l %p}", font=self._font, fill=0, anchor="ma")
+            draw.text((x_pos + 3 + (70/2), 120),
+                      f"{hour['temperature']}°", font=self._font, fill=0, anchor="md")
             icon = self._get_icon(hour['condition'])
             if icon is not None:
                 icon_image = load_icon(f"{icon}.svg", 50, 50)
@@ -107,21 +114,27 @@ class DisplayDrawer:
                 logger.warning("Couldn't find image for %s", hour['condition'])
 
             x_pos += 82
+            if x_pos >= (self.height-82):
+                break
 
         return x_pos
 
     def _draw_electric_cost(self, image, draw, x_pos):
         image.paste(self._icon_lightning, (x_pos, 130))
         x_pos += 22
-        draw.text((x_pos, 133), f"{self.data.electric_cost:.1f}¢", font=self._font, fill=0)
-        x_pos += int(draw.textlength(f"{self.data.electric_cost:.1f}¢", font=self._font))
+        draw.text((x_pos, 133), f"{
+                  self.data.electric_cost:.1f}¢", font=self._font, fill=0)
+        x_pos += int(draw.textlength(
+            f"{self.data.electric_cost:.1f}¢", font=self._font))
         x_pos += 10
         return x_pos
 
     def _draw_energy_usage(self, image, draw, x_pos):
         image.paste(self._icon_home_energy, (x_pos, 130))
         x_pos += 25
-        draw.text((x_pos, 133), f"{self.data.daily_energy:.2f} kWh", font=self._font, fill=0)
-        x_pos += int(draw.textlength(f"{self.data.daily_energy:.2f} kWh", font=self._font))
+        draw.text((x_pos, 133), f"{
+                  self.data.daily_energy:.2f} kWh", font=self._font, fill=0)
+        x_pos += int(draw.textlength(
+            f"{self.data.daily_energy:.2f} kWh", font=self._font))
         x_pos += 10
         return x_pos
--- display/updater.py
+++ display/updater.py
@@ -2,6 +2,10 @@ from .data import DisplayData
 import requests
 import logging
 import datetime
+import websockets
+import websockets.sync.client
+import pprint
+import json
 
 
 logger = logging.getLogger(__name__)
@@ -23,18 +27,60 @@ class DisplayUpdater:
     def __init__(self, data: DisplayData, ha_url: str, ha_api_key: str):
         self.data = data
         self.ha_url = ha_url
+        self._id = 0
 
+        self.__api_key = ha_api_key
         self._session = TimeoutSession()
         self._session.headers['Authorization'] = f"Bearer {ha_api_key}"
 
+    def _get_id(self):
+        self._id += 1
+        return self._id
+
+    def _call_service(self, ws: websockets.WebSocketClientProtocol, domain: str, service: str, target: str, response: bool, service_data):
+        _id = self._get_id()
+        ws.send(json.dumps({
+            "id": _id,
+            "type": "call_service",
+            "domain": domain,
+            "service": service,
+            "service_data": service_data,
+            "target": {
+                "entity_id": target
+            },
+            "return_response": response
+        }))
+        msg = {}
+        while "id" not in msg or msg["id"] != _id:
+            msg = json.loads(ws.recv())
+
+        return msg
+
     def update(self):
         logger.info("Updating display data...")
+        with websockets.sync.client.connect(f"{self.ha_url.replace('http', 'ws')}/websocket") as ws:
+            self._authorize_to_websocket(ws)
+            self._get_forecast(ws)
+            self._get_daily_low_high(ws)
         self._get_electric_cost()
-        self._get_forecast()
-        self._get_daily_low_high()
         self._get_daily_energy_usage()
         self._get_soil_moisture()
         logger.info("Done")
+
+    def _authorize_to_websocket(self, ws: websockets.WebSocketClientProtocol):
+        auth_msg = json.loads(ws.recv())
+        pprint.pprint(auth_msg)
+        if "type" not in auth_msg or auth_msg["type"] != "auth_required":
+            logger.warning("Didn't get an auth request")
+            raise RuntimeError("Didn't get auth request")
+
+        ws.send(json.dumps({
+            "type": "auth",
+            "access_token": self.__api_key
+        }))
+        msg = json.loads(ws.recv())
+        if msg["type"] != "auth_ok":
+            raise RuntimeError("Failed to auth to HA")
 
     def _get_soil_moisture(self):
         r = self._session.get(f"{self.ha_url}/states/sensor.soil_moisture")
@@ -44,44 +90,47 @@ class DisplayUpdater:
             logger.warning("Couldn't get soil moisture -- %d", r.status_code)
 
     def _get_electric_cost(self):
-        r = self._session.get(f"{self.ha_url}/states/sensor.comed_5_minute_price")
+        r = self._session.get(
+            f"{self.ha_url}/states/sensor.comed_5_minute_price")
         if r.status_code == requests.codes.ok:
             self.data.electric_cost = float(r.json()['state'])
         else:
             logger.warning("Couldn't get electric price -- %d", r.status_code)
 
-    def _get_forecast(self):
-        r = self._session.get(f"{self.ha_url}/states/weather.kpwk_hourly")
-        if r.status_code == requests.codes.ok:
-            attributes = r.json()['attributes']
-            if "forecast" in attributes:
-                data = attributes['forecast']
-                self.data.forecast = data[:3]
-            else:
-                logger.warning("Couldn't get forecast info -- No forecast in attributes?", r.status_code)
-        else:
-            logger.warning("Couldn't get forecast info -- %d", r.status_code)
+    def _get_forecast(self, ws: websockets.WebSocketClientProtocol):
+        msg = self._call_service(ws, "weather", "get_forecasts", "weather.kpwk_daynight", True, {
+            "type": "hourly"
+        })
 
-    def _get_daily_low_high(self):
-        r = self._session.get(f"{self.ha_url}/states/weather.kpwk_daynight")
-        if r.status_code == requests.codes.ok:
-            try:
-                data = r.json()['attributes']['forecast']
-                today = datetime.datetime.today()
-                for point in (0, 1):
-                    if datetime.datetime.fromisoformat(data[point]['datetime']).date() == datetime.datetime.today().date():
-                        if data[point]['is_daytime']:
-                            self.data.day_lowhigh[1] = data[point]['temperature']
-                            self.data.day_lowhigh_date = today
-                        else:
-                            self.data.day_lowhigh[0] = data[point]['temperature']
-                            self.data.day_lowhigh_date = today
-            except IndexError:
-                logger.exception("Couldn't get daynight info")
-            except KeyError:
-                logger.exception("Couldn't get daynight info")
-        else:
-            logger.warning("Couldn't get daynight info -- %d", r.status_code)
+        if "result" not in msg:
+            logger.warning("Didn't get result from weather")
+            return
+        self.data.forecast = msg['result']['response']['weather.kpwk_daynight']['forecast']
+
+    def _get_daily_low_high(self, ws):
+        msg = self._call_service(ws, "weather", "get_forecasts", "weather.kpwk_daynight", True, {
+            "type": "twice_daily"
+        })
+
+        if "result" not in msg:
+            logger.warning("Didn't get result from weather")
+            return
+
+        try:
+            data = msg['result']['response']['weather.kpwk_daynight']['forecast']
+            today = datetime.datetime.today()
+            for point in (0, 1):
+                if datetime.datetime.fromisoformat(data[point]['datetime']).date() == datetime.datetime.today().date():
+                    if data[point]['is_daytime']:
+                        self.data.day_lowhigh[1] = data[point]['temperature']
+                        self.data.day_lowhigh_date = today
+                    else:
+                        self.data.day_lowhigh[0] = data[point]['temperature']
+                        self.data.day_lowhigh_date = today
+        except IndexError:
+            logger.exception("Couldn't get daynight info")
+        except KeyError:
+            logger.exception("Couldn't get daynight info")
 
     def _get_daily_energy_usage(self):
         # TODO: Switch to websocket API to use statistics_during_period?
@@ -96,11 +145,14 @@ class DisplayUpdater:
             for previous, current in zip(data, data[1:]):
                 try:
                     if float(current['state']) > float(previous['state']):
-                        energy_sum += float(current['state']) - float(previous['state'])
+                        energy_sum += float(current['state']) - \
+                            float(previous['state'])
                     else:
-                        energy_sum += float(current['state'])  # Assumes there was a reset to zero
+                        # Assumes there was a reset to zero
+                        energy_sum += float(current['state'])
                 except ValueError:
-                    logger.warning("ValueError when calculating energy, skipping point -- %s, %s", current['state'], previous['state'])
+                    logger.warning(
+                        "ValueError when calculating energy, skipping point -- %s, %s", current['state'], previous['state'])
             self.data.daily_energy = energy_sum
         else:
             logger.warning("Couldn't get energy usage -- %d", r.status_code)
